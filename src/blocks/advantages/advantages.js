a0472e1967830c70ba0413430396868f63e71a2c
Maria <maryallbond@gmail.com>
2024-07-10 17:49:26 +0300

    redact css, html
    redact css, html, add responsivity text from 320 to 430

--- src/blocks/advantages/advantages.js
+++ src/blocks/advantages/advantages.js
@@ -1,5 +1,7 @@
 const listOfCards = document.querySelector(".advantages-list");
 
+let activeFaceOfCard, activeBackOfCard;
+
 listOfCards.addEventListener("click", rotateCard);
 
 export function rotateCard(event) {
@@ -11,6 +13,14 @@ export function rotateCard(event) {
   const faceOfCard = card.querySelector(".advantages-list-face");
   const backOfCard = card.querySelector(".advantages-list-back");
 
+  if (activeFaceOfCard && activeFaceOfCard !== faceOfCard) {
+    activeFaceOfCard.classList.remove("advantages-list-face_active");
+    activeBackOfCard.classList.remove("advantages-list-back_active");
+  }
+
   faceOfCard.classList.toggle("advantages-list-face_active");
   backOfCard.classList.toggle("advantages-list-back_active");
+
+  activeFaceOfCard = faceOfCard;
+  activeBackOfCard = backOfCard;
 }
